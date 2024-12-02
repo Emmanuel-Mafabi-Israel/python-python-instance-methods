261076ab96f923092816cdca1b6747ad40326fe4
Emmanuel-Mafabi-Israel <mafabi_emmanuel@outlook.com>
2024-12-02 12:14:40 +0300

    mafabi: project creation and initialization

---
+++ lib/dog.py
@@ -0,0 +1,10 @@
+# GLORY BE TO GOD,
+# PYTHON PROGRAMMING - PYTHON - INSTANCE METHODS
+# BY ISRAEL MAFABI EMMANUEL
+
+class Dog:
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
---
+++ lib/person.py
@@ -0,0 +1,16 @@
+# GLORY BE TO GOD,
+# PYTHON PROGRAMMING - PYTHON - INSTANCE METHODS
+# BY ISRAEL MAFABI EMMANUEL
+
+class Person:
+    # instance method
+    # Walk and Talk
+    def walk(self):
+        print("The person is walking.")
+
+    def talk(self):
+        print("Hello World!")
+
+person = Person()
+person.walk()
+person.talk()
